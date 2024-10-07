feb0cda0854aaefdbe7bf1a846ad69d990ed7c1d
Rubén Boquete Donaire <125016434+Rubenbd74@users.noreply.github.com>
2024-10-07 20:30:32 +0200

    a

---
+++ practicas/practica1/09.php
@@ -0,0 +1,5 @@
+<?php
+
+/* Escriba un programa a partir de un número entero generado de forma aleatoria 
+indique si es primo. Un número primo es aquel que es divisible por el mismo y la 
+unidad. */
---
+++ practicas/practica1/10.php
@@ -0,0 +1,4 @@
+<?php
+
+/* Generar todos los pares de números formados por combinaciones de dígitos del 1 
+al 9, siendo además los dos componentes del par distintos */
