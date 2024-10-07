6e76b55e2c1775db3f17edcc0f45c808180366b9
Rubén Boquete Donaire <125016434+Rubenbd74@users.noreply.github.com>
2024-10-07 20:58:43 +0200

    a

--- practicas/practica1/07.php
+++ practicas/practica1/07.php
@@ -3,3 +3,13 @@
 /* Hacer un programa que calcule todos los números primos entre 1 y 50 y los 
 muestre por pantalla. Un número primo es un número entero que sólo es 
 divisible por 1 y por sí mismo. */
+
+for ($i = 1; $i < 50; $i++) {
+    if ($i%50==0){
+        $div += $div+1;
+    }
+}
+
+if ($div <= 2){
+    echo $i;
+}
