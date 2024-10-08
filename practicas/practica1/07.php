3d9e0b3ade40ccde7a4654a3ac5f18aa41fbfdd6
Ruben Boquete <rubenbd74@educastur.es>
2024-10-08 09:45:48 +0200

    a

--- practicas/practica1/07.php
+++ practicas/practica1/07.php
@@ -4,12 +4,14 @@
 muestre por pantalla. Un número primo es un número entero que sólo es 
 divisible por 1 y por sí mismo. */
 
-for ($i = 1; $i < 50; $i++) {
+
+for ($i = 1; $i <= 50; $i++) {
+    $div = 0;
+    echo $i%50, "</br>";
     if ($i%50==0){
         $div += $div+1;
     }
-}
-
-if ($div <= 2){
-    echo $i;
+    if ($div <= 2){
+        echo $i, "</br>";
+    }
 }
