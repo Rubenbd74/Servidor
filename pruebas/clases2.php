79decb9d612747373f1e071570403f6b2bf01495
Ruben Boquete <rubenbd74@educastur.es>
2024-10-10 14:13:25 +0200

    a

---
+++ pruebas/clases2.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'Persona.php';
+
+//Creamos el objeto.
+$persona = new Persona('Fernando', 'Gaitan', 26);
+//Seteamos las propiedades.
+//$persona->nombre = 'Fernando';
+//$persona->apellido = 'Gaitan';
+//$persona->edad = 26;
+//Mostramos el resultado de las propiedades.
+echo 'Nombre: ' . $persona->nombre . '<br />';
+echo 'Apellido: ' . $persona->apellido . '<br />';
+echo 'Edad: ' . $persona->edad . '<br />';
+echo 'Saludo: ' . $persona->saludar();
