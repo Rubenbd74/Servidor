ed13b4c3fcc02843e21972b0c16a448364cbb10c
Ruben Boquete <rubenbd74@educastur.es>
2024-10-16 13:25:37 +0200

    a

--- practicas/U3practica1/25.php
+++ practicas/U3practica1/25.php
@@ -6,7 +6,34 @@ Los valores serán los siguientes:
 
 En Madrid: nombre Pedro, edad 32, teléfono 91-999.99.99
 En Barcelona: nombre Susana, edad 34, teléfono 93-000.00.00
-En Tolria: nombre Sonia, edad 42, teléfono 925-09.09.09
+En Toledo: nombre Sonia, edad 42, teléfono 925-09.09.09
 
 Haz un recorrido del array multidimensional mostrando los valores de tal manera 
 que nos muestre en cada ciudad que amigos tiene */
+
+$amigos = [
+    "Madrid" => [
+        "nombre" => "Pedro",
+        "edad" => "32",
+        "teléfono" => "91-999.99.99"
+        ],
+
+    "Barcelona"=> [
+        "nombre" => "Susana",
+        "edad" => "34",
+        "teléfono" => "93-000.00.00"
+    ],
+
+    "Toledo" => [
+        "nombre" => "Sonia",
+        "edad" => "42",
+        "teléfono" => "925-09.09.09"
+    ]
+];
+
+foreach ($amigos as $key => $value) {
+    echo "</br>",$key, ": </br></br>";
+    foreach ($value as $key2 => $value2) {
+        echo $key2, ":", $value2, "</br>";
+    }
+}
