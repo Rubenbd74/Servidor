384a547806d36b3909dfcc5112a6e6a2b3ce9145
Ruben Boquete <rubenbd74@educastur.es>
2024-10-21 14:22:58 +0200

    a

--- pruebas/ficheros/formradio.php
+++ pruebas/ficheros/formradio.php
@@ -1,7 +1,7 @@
 <form method="POST" action="resultadoRadio.php">
-    <input type="radio" name="MyRadio" value="14">Menos de 14 años<br> //This one is automatically checked when the user opens the page
-    <input type="radio" name="MyRadio" value="15">de 15 a 20 años
-    <input type="radio" name="MyRadio" value="21">de 21 a 40 años
-    <input type="radio" name="MyRadio" value="60">Má de 60 años
-    <input type="submit" value="Resultado" name="Resultado"> //This button opens Result.php
+    <input type="radio" name="MyRadio" value="14">Menos de 14 años<br>
+    <input type="radio" name="MyRadio" value="15">de 15 a 20 años<br>
+    <input type="radio" name="MyRadio" value="21">de 21 a 40 años<br>
+    <input type="radio" name="MyRadio" value="60">Má de 60 años<br>
+    <input type="submit" value="Resultado" name="Resultado"> 
 </form>
