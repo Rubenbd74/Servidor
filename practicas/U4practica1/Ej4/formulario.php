dd21890dac0595cca89cf720d233f6f307dbc51d
Ruben Boquete <rubenbd74@educastur.es>
2024-10-24 12:00:34 +0200

    a

--- practicas/U4practica1/Ej4/formulario.php
+++ practicas/U4practica1/Ej4/formulario.php
@@ -21,15 +21,15 @@
 </form>
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $mes = $_POST["mes"];
-  $anio = $_POST["anio"];
-  $fecha_inicio = new DateTime("$anio-$mes-01");
-  $num_dias = $fecha_inicio->format('t');
-  $dia_semana = $fecha_inicio->format('N'); // Utilizamos 'N' en lugar de 'w'
-}
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $mes = $_POST["mes"];
+    $anio = $_POST["anio"];
+    $fecha_inicio = new DateTime("$anio-$mes-01");
+    $num_dias = $fecha_inicio->format('t');
+    $dia_semana = $fecha_inicio->format('N');
+  }
 
-if (isset($mes) && isset($anio)) {
+  if (isset($mes) && isset($anio)) {
     echo "<h3>Calendario del mes $mes del $anio</h3>";
     echo "<table border='1' cellpadding='5' cellspacing='0'>";
     echo "<tr>";
@@ -41,23 +41,23 @@ if (isset($mes) && isset($anio)) {
     echo "<th style='background-color: #0000ff; color: #ffffff'>Sábado</th>";
     echo "<th style='background-color: #0000ff; color: #ffffff'>Domingo</th>";
     echo "</tr>";
-    
-      $fila = 1;
-      $dia_actual = 1;
-      for ($i = 1; $i < $dia_semana; $i++) {
-        echo "<td>&nbsp;</td>";
-      }
-      for ($i = 1; $i <= $num_dias; $i++) {
-        echo "<td>$i</td>";
-        if (($i + $dia_semana - 1) % 7 == 0) {
-          echo "</tr>";
-          $fila++;
-          if ($i < $num_dias) {
-            echo "<tr>";
-          }
+      
+    $fila = 1;
+    $dia_actual = 1;
+    for ($i = 1; $i < $dia_semana; $i++) {
+      echo "<td>&nbsp;</td>";
+    }
+    for ($i = 1; $i <= $num_dias; $i++) {
+      echo "<td>$i</td>";
+      if (($i + $dia_semana - 1) % 7 == 0) {
+        echo "</tr>";
+        $fila++;
+        if ($i < $num_dias) {
+          echo "<tr>";
         }
       }
-      echo "</tr>";
-      echo "</table>";
     }
-    ?>
+    echo "</tr>";
+    echo "</table>";
+  }
+?>
