bc3ddf9bf9488dd49c306ce957c8494c71e07cee
Rubén Boquete Donaire <125016434+Rubenbd74@users.noreply.github.com>
2024-11-04 20:40:35 +0100

    a

--- pruebas/simon.php
+++ pruebas/simon.php
@@ -1,3 +1,9 @@
+<script>
+  $colores = ["blue", "yellow", "red", "green"];
+  $colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+  return $colorAleatorio;
+</script>
+
 <html>
     <svg width="200" height="200">
         <circle cx="100" cy="100" r="50" fill= <?php $colorAleatorio ?>/>
@@ -6,26 +12,3 @@
         <input type= "submit" value="Jugar">
     </form>
 </html>
-<?php
- 
-?>
-<svg width="200" height="200">
-  <circle cx="100" cy="100" r="50" fill="#FF0000" />
-</svg>
-
-<script>
-  // Definimos los colores posibles
-  var colores = ["#FF0000", "#0000FF", "#008000", "#FFFF00"];
-
-  // Seleccionamos un color aleatorio
-  var colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
-
-  // Obtenemos el elemento SVG
-  var svg = document.querySelector("svg");
-
-  // Obtenemos el elemento círculo
-  var circulo = svg.querySelector("circle");
-
-  // Cambiamos el color del círculo
-  circulo.setAttribute("fill", colorAleatorio);
-</script>
