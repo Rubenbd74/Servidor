7a4ed7bc2100003fe262e992fd1f057ff8ea6c30
Rubén Boquete Donaire <125016434+Rubenbd74@users.noreply.github.com>
2024-11-04 20:51:36 +0100

    a

--- pruebas/simon.php
+++ pruebas/simon.php
@@ -6,7 +6,7 @@
 
 <html>
     <svg width="200" height="200">
-        <circle cx="100" cy="100" r="50" fill= <?php $colorAleatorio ?>/>
+        <circle cx="100" cy="100" r="50" fill= <?php $colorAleatorio ?> />
     </svg>
     <form>
         <input type= "submit" value="Jugar">
