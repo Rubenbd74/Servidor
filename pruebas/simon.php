ed348e88eca1915636717f07f9a2486bec56f5dc
Ruben Boquete <rubenbd74@educastur.es>
2024-11-06 13:31:53 +0100

    a

--- pruebas/simon.php
+++ pruebas/simon.php
@@ -1,18 +1,21 @@
 <html>
     <svg width="200" height="200">
-        <circle cx="100" cy="100" r="50" fill= <?php $colorAleatorio ?>/>
+        <circle cx="100" cy="100" r="50" fill=" <?php $colorAleatorio ?>" />
     </svg>
     <form>
         <input type= "submit" value="Jugar">
     </form>
 </html>
 <?php
- 
+  /* Hacer un random de 0 a 3 y luego un switch para cada numero asociandolos a un color
+     Hacer un circulo con el style en html y por color usar la variable anterior
+     <input type="hidden" name="color" value="<?php echo $color; ?>
+     Si existe $POST color igualar $SESSION a $POST
+     Si existe $POST tempcolor igualar a $SESSION tempcolor
+     Si no existe tempcolor = black
+     ">
+  */
 ?>
-<svg width="200" height="200">
-  <circle cx="100" cy="100" r="50" fill="#FF0000" />
-</svg>
-
 <script>
   // Definimos los colores posibles
   var colores = ["#FF0000", "#0000FF", "#008000", "#FFFF00"];
