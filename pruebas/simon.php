01e3c9a3631e2fce0b7411651a44d8c57b3aa8b5
Ruben Boquete <rubenbd74@educastur.es>
2024-11-06 14:25:39 +0100

    a

--- pruebas/simon.php
+++ pruebas/simon.php
@@ -1,12 +1,43 @@
+<?php
+
+  $randcolores = array();
+  for ($i = 0; $i < 4; $i++) {
+      $randcolores[] = rand(0, 3);
+  }
+
+  $colorAleatorio = array();
+  foreach ($randcolores as $valor){
+    switch($valor){
+      case 0:
+        $colorAleatorio[] = "yellow";
+      case 1:
+        $colorAleatorio[] = "blue";
+      case 2:
+        $colorAleatorio[] = "red";
+      case 3:
+        $colorAleatorio[] = "green";  
+    }
+  }
+
+?>
+
 <html>
-    <svg width="200" height="200">
-        <circle cx="100" cy="100" r="50" fill=" <?php $colorAleatorio ?>" />
-    </svg>
+    <div style="display: flex; justify-content: space-between;">
+      <div style="width: 50px; height: 50px; border-radius: 50%; background-color: <?php echo $colorAleatorio[0] ?>;"></div>
+      <div style="width: 50px; height: 50px; border-radius: 50%; background-color: <?php echo $colorAleatorio[1] ?>;"></div>
+      <div style="width: 50px; height: 50px; border-radius: 50%; background-color: <?php echo $colorAleatorio[2] ?>;"></div>
+      <div style="width: 50px; height: 50px; border-radius: 50%; background-color: <?php echo $colorAleatorio[3] ?>;"></div>
+    </div>
+    
     <form>
         <input type= "submit" value="Jugar">
     </form>
 </html>
 <?php
+
+  /*<svg width="200" height="200">
+        <circle cx="100" cy="100" r="50" fill=" <?php $colorAleatorio ?>" />
+    </svg>-->*/
   /* Hacer un random de 0 a 3 y luego un switch para cada numero asociandolos a un color
      Hacer un circulo con el style en html y por color usar la variable anterior
      <input type="hidden" name="color" value="<?php echo $color; ?>
@@ -16,19 +47,3 @@
      ">
   */
 ?>
-<script>
-  // Definimos los colores posibles
-  var colores = ["#FF0000", "#0000FF", "#008000", "#FFFF00"];
-
-  // Seleccionamos un color aleatorio
-  var colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
-
-  // Obtenemos el elemento SVG
-  var svg = document.querySelector("svg");
-
-  // Obtenemos el elemento círculo
-  var circulo = svg.querySelector("circle");
-
-  // Cambiamos el color del círculo
-  circulo.setAttribute("fill", colorAleatorio);
-</script>
