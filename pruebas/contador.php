e22b593c7400b7247289c710132f0c5955d8513d
Rubén Boquete Donaire <125016434+Rubenbd74@users.noreply.github.com>
2024-11-11 14:19:47 +0100

    a

--- pruebas/contador.php
+++ pruebas/contador.php
@@ -1,41 +1,37 @@
 <?php
     session_start();
 
+    // Inicializa el contador
     if (!isset($_SESSION['contador'])) {
         $_SESSION['contador'] = 0;
     }
 
-
-    if (isset($_POST['mas'])) {
-        $_SESSION['contador']++;
-    }
-
-    // Decrementa el contador
-    if (isset($_POST['menos'])) {
-        $_SESSION['contador']--;
-    }
-
-    // Cierra la sesión
-    if (isset($_POST['cerrar'])) {
-        session_unset(); // Limpia todas las variables de sesión
-        session_destroy(); // Destruye la sesión
-        header("Location: " . $_SERVER['PHP_SELF']); // Redirige a la misma página para refrescar
-        exit;
+    // Acciones de los botones
+    if (isset($_POST['accion'])) {
+        switch ($_POST['accion']) {
+            case 'mas': // Aumentar
+                $_SESSION['contador']++;
+                break;
+            case 'menos': // Disminuir
+                $_SESSION['contador']--;
+                break;
+            case 'cerrar': //Cerrar
+                session_unset(); // Limpia todas las variables de sesión
+                session_destroy(); // Destruye la sesión
+                header("Location: " . $_SERVER['PHP_SELF']); // Redirige a la misma página
+                exit;
+        }
     }
 ?>
 
+
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Contador Manual en PHP</title>
-    </head>
     <body>
         <h1>Contador: <?php echo $_SESSION['contador']; ?></h1>
         <form method="post">
-            <button type="submit" name="menos">-</button>
-            <button type="submit" name="mas">+</button>
-            <button type="submit" name="cerrar">Cerrar Sesión</button>
+            <button type="submit" name="boton" value="menos">-</button>
+            <button type="submit" name="boton" value="mas">+</button>
+            <button type="submit" name="boton" value="cerrar">Cerrar</button>
         </form>
     </body>
 </html>
