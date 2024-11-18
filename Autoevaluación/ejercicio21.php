a4c1f55bea9ea656feb0a134511ee64029ad3f47
Ruben Boquete <rubenbd74@educastur.es>
2024-11-18 13:24:38 +0100

    a

--- Autoevaluación/ejercicio21.php
+++ Autoevaluación/ejercicio21.php
@@ -6,6 +6,8 @@ $solucion = $_SESSION['decimal'];
 
 if ($respuesta == $solucion) {
   echo "¡Correcto! El número decimal es $solucion.";
+  echo "<br><a href='ejercicio2.php'>Jugar de nuevo</a>";
 } else {
   echo "Error. El número decimal correcto es $solucion.";
+  echo "<br><a href='ejercicio2.php'>Jugar de nuevo</a>";
 }
