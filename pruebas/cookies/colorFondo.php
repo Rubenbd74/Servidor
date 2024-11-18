521979e8a3999ea7774c58f9830b8d8f72e66e70
Ruben Boquete <rubenbd74@educastur.es>
2024-11-18 14:25:14 +0100

    a

---
+++ pruebas/cookies/colorFondo.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<form>
+    <label>Seleccione de que color desea que sea la web de ahora en adelante:</label>
+    <input type="radio" id="red" name="color" value="red">
+    <label for="red">Rojo</label>
+    <input type="radio" id="green" name="color" value="green">
+    <label for="green">Verde</label>
+    <input type="radio" id="blue" name="color" value="blue">
+    <label for="blue">Azul</label>
+    <input type="submit" value="Crear cookie">
+</form>
