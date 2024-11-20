18b460bd7ed56f991d71030aad549a43c526e838
Ruben Boquete <rubenbd74@educastur.es>
2024-11-20 12:00:04 +0100

    a

--- pruebas/cookies/colorFondo.php
+++ pruebas/cookies/colorFondo.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 // Verificar si la cookie existe
 if (isset($_COOKIE['backgroundColor'])) {
     $backgroundColor = $_COOKIE['backgroundColor'];
@@ -17,12 +19,12 @@ if (isset($_COOKIE['backgroundColor'])) {
   echo '<style>body { background-color: ' . $backgroundColor . '; }</style>';
   ?>
 <form>
-    <label>Seleccione de que color desea que sea la web de ahora en adelante:</label>
+    <label>Seleccione de que color desea que sea la web de ahora en adelante:</label><br>
     <input type="radio" id="red" name="color" value="red">
-    <label for="red">Rojo</label>
+    <label for="red">Rojo</label><br>
     <input type="radio" id="green" name="color" value="green">
-    <label for="green">Verde</label>
+    <label for="green">Verde</label><br>
     <input type="radio" id="blue" name="color" value="blue">
-    <label for="blue">Azul</label>
+    <label for="blue">Azul</label><br>
     <input type="submit" value="Crear cookie">
 </form>
