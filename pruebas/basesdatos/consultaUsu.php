c3fd6e2ece759087d0ac771e2672c4ec10af67bd
Ruben Boquete <rubenbd74@educastur.es>
2024-11-28 12:56:39 +0100

    a

--- pruebas/basesdatos/consultaUsu.php
+++ pruebas/basesdatos/consultaUsu.php
@@ -6,7 +6,8 @@
     <title>Sesiones</title>
 </head>
 <body>
-    <?php
+    
+<?php
      require_once 'login.php';
      $conn = new mysqli($hn, $un, $pw, $db);
      if ($conn->connect_error) die("Fatal Error");
@@ -16,29 +17,30 @@
         $input_contra = $_POST['contra'];
     
     
-     $query = "SELECT usu,contra FROM usuarios WHERE usu='$input_usu' AND 
-     contra='$input_contra'";
-     $result = $conn->query($query);
-     if ($result && ($result->num_rows >0)) {
-     echo'Se ha inciado sesion correctamente';
-     $rows = $result->fetch_assoc(); 
-  
-     echo '<br>';
-     echo '<strong>Usuario: </strong>' . htmlspecialchars($rows['usu']) . '<br>';
-     echo '<strong>Contraseña: </strong>' . htmlspecialchars($rows['contra']) . '<br>';
-     
-    }
+        $query = "SELECT usu,contra FROM usuarios WHERE usu='$input_usu' AND 
+        contra='$input_contra'";
+        $result = $conn->query($query);
+        
+        if ($result && ($result->num_rows >0)) {
+            echo'Se ha inciado sesion correctamente';
+            $rows = $result->fetch_assoc(); 
     
-    else {
-
-        echo 'Fatal error.';
-
+            echo '<br>';
+            echo '<strong>Usuario: </strong>' . htmlspecialchars($rows['usu']) . '<br>';
+            echo '<strong>Contraseña: </strong>' . htmlspecialchars($rows['contra']) . '<br>';
+        
         }
         
+        else {
+
+            echo 'No existe el usuario o la contraseña es incorrecta';
+
+        }
+            
     }
     $conn->close();
+?>
 
-    ?>
 <form action ="#" method ="post">
 <label>Usuario</label>
 <input type="text" name="usu">
