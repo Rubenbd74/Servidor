5b8cf3b5ca77af9f4d056c641ed6f4bafd3bea31
Ruben Boquete <rubenbd74@educastur.es>
2024-12-02 13:24:15 +0100

    a

--- pruebas/basesdatos/respuesta.php
+++ pruebas/basesdatos/respuesta.php
@@ -8,14 +8,16 @@ if (isset($_SESSION['colores_adivinar'])) {
 
 // Verificar si se ha enviado el formulario
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Obtener el índice del color seleccionado
-    $indiceSeleccionado = $_POST['0'];
+    // Obtener los colores seleccionados
+    $coloresSeleccionados = $_POST;
 
-    // Verificar si el color seleccionado es correcto
-    if ($indiceSeleccionado === array_search($colorAdivinar[0], $_SESSION['colores'])) {
-        echo "¡Correcto! El color era " . $colorAdivinar[0];
-    } else {
-        echo "¡Incorrecto! El color era " . $colorAdivinar[0];
+    // Verificar si alguno de los colores seleccionados es correcto
+    foreach ($coloresSeleccionados as $color) {
+        if ($color === $colorAdivinar[0]) {
+            echo "¡Correcto! El color era " . $colorAdivinar[0];
+        } else {
+            echo "¡Incorrecto! El color era " . $colorAdivinar[0];
+        }
     }
 }
 
