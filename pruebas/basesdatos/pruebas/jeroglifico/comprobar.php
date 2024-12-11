644301c4e93211334bc9dc2bf2577dbb9a1e1877
Ruben <rubenbd74@educastur.es>
2024-12-11 20:27:40 +0100

    a

--- pruebas/basesdatos/pruebas/jeroglifico/comprobar.php
+++ pruebas/basesdatos/pruebas/jeroglifico/comprobar.php
@@ -1,7 +1,7 @@
 <?
 require_once 'conexion.php';
 require_once 'mostrar_cartas.php';
-session_start();
+
 if(isset($_POST['comprobar'])) {
     $pareja1 = $_POST['pareja1'] - 1; // Restamos 1 para que coincida con el índice del array
     $pareja2 = $_POST['pareja2'] - 1;
