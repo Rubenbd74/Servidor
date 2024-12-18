e80c07ef8a74bf3e07871cf6aa5d39716eb8ee5b
Ruben Boquete <rubenbd74@educastur.es>
2024-12-18 14:00:54 +0100

    a

---
+++ pruebas/basesdatos/ficheros/explicacion/upload.php
@@ -0,0 +1,15 @@
+<?php
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+    // Comprobamos si es un archivo de texto
+    if($imageFileType != "txt" ) {
+        echo "El fichero no es un archivo de texto, .txt";
+        $uploadOk = 0;
+    }
+    else{
+        echo "El fichero es un archivo de texto, .txt";
+        $uploadOk = 1;
+    }
