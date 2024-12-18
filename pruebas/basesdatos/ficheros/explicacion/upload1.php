289d9c9ffa38922be2b6a31fca17368ff032cc9a
Ruben Boquete <rubenbd74@educastur.es>
2024-12-18 14:07:54 +0100

    a

--- pruebas/basesdatos/ficheros/explicacion/upload1.php
+++ pruebas/basesdatos/ficheros/explicacion/upload1.php
@@ -5,17 +5,16 @@
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-
     // Comprobamos si es una imagen real o falsa
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "El fichero es una imagen - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } 
-    else {
-        echo "El fichero no es una imagen.";
-        $uploadOk = 0;
-    }
+     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+     if($check !== false) {
+     echo "El fichero es una imagen - " . $check["mime"] . ".";
+     $uploadOk = 1;
+     } else {
+     echo "El fichero no es una imagen.";
+     $uploadOk = 0;
+     }
+
 
 /*Aclaraciones sobre el script
 $ target_dir = "uploads/" - indica el directorio donde se coloca el archivo
