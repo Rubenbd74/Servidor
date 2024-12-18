6e118f4667dc4715f25e1a41f6f39f94126f060d
Ruben Boquete <rubenbd74@educastur.es>
2024-12-18 14:01:47 +0100

    a

--- pruebas/basesdatos/ficheros/explicacion/upload1.php
+++ pruebas/basesdatos/ficheros/explicacion/upload1.php
@@ -1,7 +1,7 @@
 <!-- El "upload.php" archivo contiene el código para cargar un archivo:-->
 
 <?php
-    $target_dir = "uploads";
+    $target_dir = "uploads/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
