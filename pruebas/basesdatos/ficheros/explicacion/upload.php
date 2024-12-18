c3b1e2ea151c9cfd1eb068b87a9105ef11f6af36
Ruben Boquete <rubenbd74@educastur.es>
2024-12-18 13:42:07 +0100

    a

--- pruebas/basesdatos/ficheros/explicacion/upload.php
+++ pruebas/basesdatos/ficheros/explicacion/upload.php
@@ -17,8 +17,7 @@
         $uploadOk = 0;
     }
 
-/*
-Aclaraciones sobre el script
+/*Aclaraciones sobre el script
 $ target_dir = "uploads/" - indica el directorio donde se coloca el archivo
 $ Target_file especifica la ruta del archivo sea cargado
 $ uploadOk = 1 no se utilizatodavía (will be used later)
@@ -26,4 +25,4 @@ $ ImageFileType tiene la extensión de archivo del archivo
 A continuación, compruebe si el archivo de imagen es una imagen real o una imagen 
 falsa
 Nota: tendrá que crear un nuevo directorio llamado "uploads" en el directorio 
-donde "upload.php" reside el archivo. Los archivos subidos se guardarán allí.
+donde "upload.php" reside el archivo. Los archivos subidos se guardarán allí.*/
