0b2055c0c782302951031ec93281d9a2b8e34aa8
Ruben Boquete <rubenbd74@educastur.es>
2025-02-06 14:17:19 +0100

    a

--- MIVIDEOCLUB/routes/web.php
+++ MIVIDEOCLUB/routes/web.php
@@ -2,11 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', function () {   //public/
     echo "Hola Mundo";
     //return view('welcome'); //Nos muestra la vista MIVIDEOCLUB/resources/views/welcome.blade.php
 });
 
-Route::get('pagina1', function () {
+Route::get('pagina1', function () { //public/pagina1
     return ('Estas en la pagina 1');
+});
+
+Route::get('pagina2/{id}', function($id)   //public/pagina2/1
+{
+return 'Usuario '.$id;
+});
+
+Route::get('pagina3/{name?}', function($name = null)    //public/pagina3
+{
+return $name;
+});
+// También podemos poner algún valor por defecto...
+Route::get('pagina3/{name?}', function($name = 'Javi')
+{
+return $name;
 });
