bec7040a688902e62349023dc4e34c6838c84fad
Ruben Boquete <rubenbd74@educastur.es>
2025-02-06 13:54:43 +0100

    a

--- MIVIDEOCLUB/routes/web.php
+++ MIVIDEOCLUB/routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Hola Mundo";
+    //return view('welcome'); //Nos muestra la vista MIVIDEOCLUB/resources/views/welcome.blade.php
+});
+
+Route::get('pagina1', function () {
+    return ('Estas en la pagina 1');
 });
