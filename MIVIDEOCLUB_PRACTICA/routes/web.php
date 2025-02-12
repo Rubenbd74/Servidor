db8a42092557f3e402ba2574e80f45f39f5bc7fb
Ruben Boquete <rubenbd74@educastur.es>
2025-02-12 13:58:10 +0100

    a

--- MIVIDEOCLUB_PRACTICA/routes/web.php
+++ MIVIDEOCLUB_PRACTICA/routes/web.php
@@ -2,6 +2,79 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+/*Route::get('/', function () {   //public/
+    echo "Hola Mundo";
+    //return view('welcome'); //Nos muestra la vista MIVIDEOCLUB/resources/views/welcome.blade.php
 });
+
+Route::get('pagina1', function () { //public/pagina1
+    return ('Estas en la pagina 1');
+});
+
+Route::get('pagina2/{id}', function($id)   //public/pagina2/1
+{
+return 'Usuario '.$id;
+});
+
+Route::get('pagina3/{name?}', function($name = null)    //public/pagina3/elnombre  //el interrogante hace que sea opcional
+{
+return 'Hola '. $name;
+});
+
+Route::get('pagina3/{name?}', function($name = 'Rubén')  // También podemos poner algún valor por defecto... public/pagina3/
+{
+return 'Hola '. $name;
+});
+
+//Podemos usar expresiones regulares para validar los parámetros
+
+Route::get('pagina4/{name}', function($name)
+{
+//
+})
+->where('name', '[A-Za-z]+');
+
+Route::get('pagina4/{id}', function($id)
+{
+//
+})
+->where('id', '[0-9]+');
+
+// Si hay varios parámetros podemos validarlos usando un array:
+Route::get('pagina4/{id}/{name}', function($id, $name)
+{
+//
+})
+->where(array('id' => '[0-9]+', 'name' => '[A-Za-z]+'));*/
+
+Route::get('/', function () {
+    //return 'Pantalla principal';
+    return view('home');
+});
+
+Route::get('/login', function () {
+    //return 'Login usuario';
+    return view('login');
+});
+Route::get('/logout', function () {
+    //return 'Logout del usuario';
+    return view('home');
+});
+Route::get('/catalog', function () {
+    //return 'Listado de peliculas';
+    return view('catalog.index');
+});
+
+Route::get('/catalog/show/{id}', function ($id) {
+    //return 'Vista detalle película '. $id;
+    return view('catalog.show', array('id'=>$id));
+});
+
+Route::get('/catalog/create', function () {
+    //return 'Añadir película';
+    return view('catalog.create');
+});
+
+Route::get('catalog/edit/{id}', function ($id) {
+    return 'Modificar pelicula '. $id;});
+    return view('catalog.edit', array('id'=>$id));
