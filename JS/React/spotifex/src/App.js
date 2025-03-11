0de7cb4d346e34b54a0e09c69acb1f76f3b58988
Ruben Boquete <rubenbd74@educastur.es>
2025-03-11 10:10:51 +0100

    a

---
+++ JS/React/spotifex/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SpotiMain from './Components/SpotiMain';
+
+function App() {
+  return (
+    <div className="App">
+      <SpotiMain />
+    </div>
+  );
+}
+
+export default App;
